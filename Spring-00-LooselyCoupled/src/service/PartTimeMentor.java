a1a1456fed4a4243ed596afab03117d873c8c6e7
Emrah Bulut <bltemrah@outlook.com>
2020-11-01 08:32:23 +0000

    Tightly Coupled

--- Spring-00-LooselyCoupled/src/service/PartTimeMentor.java
+++ Spring-00-LooselyCoupled/src/service/PartTimeMentor.java
@@ -3,8 +3,11 @@ package service;
 import implementation.Mentor;
 
 public class PartTimeMentor implements Mentor {
+
     @Override
     public void createAccount() {
+
         System.out.println("Part Time Mentor Account is created...");
     }
+
 }
