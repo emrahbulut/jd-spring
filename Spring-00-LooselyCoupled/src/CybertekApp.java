f73f7fc5f5d76036c7c77f872d3f69a866aac543
Emrah Bulut <bltemrah@outlook.com>
2020-11-01 10:18:35 +0000

    Loosely Coupled

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,21 @@
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime = new FullTimeMentor();
+
+        PartTimeMentor partTime = new PartTimeMentor();
+
+        MentorAccount partMentor = new MentorAccount(fullTime);
+        MentorAccount fullMentor = new MentorAccount (partTime);
+
+        partMentor.manageAccount();
+        fullMentor.manageAccount();
+    }
+
+}
+
--- Spring-00-TightlyCoupled/src/Mentor.java
+++ Spring-00-TightlyCoupled/src/Mentor.java
@@ -1,5 +1,6 @@
 public class Mentor {
 
+//    Composition
     FullTimeMentor fullTimeMentor;
     PartTimeMentor partTimeMentor;
     //MI
