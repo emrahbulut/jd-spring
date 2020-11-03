9f97f9e82e0e829c0aa91d5f994ca8aef1e065aa
Emrah Bulut <bltemrah@outlook.com>
2020-11-03 20:51:55 +0000

    DI-Constructor Injection

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,24 @@
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+//        Old way
+//        Course course = new Java();
+
+//        INTERFACE   =     OBJECT                  DOWN CASTING
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
+
+    }
+
+
+}
