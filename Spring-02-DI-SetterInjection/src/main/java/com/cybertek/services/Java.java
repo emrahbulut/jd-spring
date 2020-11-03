9f28b931ef244dfe7fd4b7af2c071e28705cc0b1
Emrah Bulut <bltemrah@outlook.com>
2020-11-03 22:27:47 +0000

    DI-Setter  Injection

--- Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -2,23 +2,32 @@ package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSessions;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+//@Getter
+//@Setter
+//@ToString
+//If we use Data, it includes all above, and no need to create getter and setter method
+@Data
 public class Java implements Course {
 
-    private OfficeHours officeHours;
+    private ExtraSessions extraSessions;
 
-    public OfficeHours getOfficeHours() {
-        return officeHours;
-    }
-
-    public void setOfficeHours(OfficeHours officeHours) {
-        this.officeHours = officeHours;
-    }
+//    public OfficeHours getOfficeHours() {
+//        return officeHours;
+//    }
+//
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
 
     @Override
     public void getTeachingHours() {
 
-        System.out.println("Weekly Teaching Hours: " + (20+ officeHours.getHours()));
+        System.out.println("Weekly Teaching Hours: " + (20+ extraSessions.getHours()));
 
     }
 }
