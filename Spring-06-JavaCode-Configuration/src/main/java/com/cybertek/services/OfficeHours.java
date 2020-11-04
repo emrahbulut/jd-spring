e02970f4245e9fe16d379bd4262c2eb7570b987f
Emrah Bulut <bltemrah@outlook.com>
2020-11-04 22:55:42 +0000

    Java Code - Configuration

---
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/OfficeHours.java
@@ -0,0 +1,14 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.stereotype.Component;
+
+
+public class OfficeHours implements ExtraSessions {
+
+    @Override
+    public int getHours() {
+
+        return 7;
+    }
+}
